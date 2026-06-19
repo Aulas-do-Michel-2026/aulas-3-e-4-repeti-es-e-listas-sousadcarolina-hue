31a4325f5a578339841127ce1aff5032a4c5dc02
sousadcarolina-hue <sousad.carolina@gmail.com>
2026-06-18 21:36:32 -0300

    Update exercicio_4.py

--- exercicio_4.py
+++ exercicio_4.py
@@ -34,3 +34,13 @@ primeira_lista = [*map(int, input("Digite a sua primeira lista (separando os nú
 segunda_lista = [*map(int, input("Digite a sua segunda lista (separando os números por vírgula): ").split(","))]
 
 # Fazer a partir daqui
+
+maior_primeira = max(primeira_lista)
+maior_segunda = max(segunda_lista)
+
+if maior_primeira > maior_segunda:
+    print("Primeira")
+elif maior_segunda > maior_primeira:
+    print("Segunda")
+else:
+    print("Ambas")
